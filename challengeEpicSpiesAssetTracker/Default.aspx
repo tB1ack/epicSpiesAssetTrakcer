5cd8ca8eccc0bb928b6cc18f2d8c4519a10a599c
tB1ack <31832634+tB1ack@users.noreply.github.com>
2018-02-08 19:19:41 -0800

    Add files via upload

---
+++ challengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="challengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 150px;
+            height: 184px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="Epic Spies Logo" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+            <h2 class="auto-style2">Asset Performance Tracker</h2>
+            <br />
+            Asset Name: <asp:TextBox ID="assetNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="electionsRiggedTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="subterfugePerformedTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addAsssetButton" runat="server" OnClick="addAsssetButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ challengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] subterfuge = new int[0];
+
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Subterfuge", subterfuge);
+            }
+        }
+
+        protected void addAsssetButton_Click(object sender, EventArgs e)
+        {
+            string[] assets = (string[]) ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] subterfuge = (int[])ViewState["Subterfuge"];
+
+            int newLength = assets.Length + 1;
+
+            Array.Resize(ref assets, newLength);
+            Array.Resize(ref elections, newLength);
+            Array.Resize(ref subterfuge, newLength);
+
+            int newIndex = assets.GetUpperBound(0);
+
+            assets[newIndex] = assetNameTextBox.Text;
+            elections[newIndex] = int.Parse(electionsRiggedTextBox.Text);
+            subterfuge[newIndex] = int.Parse(subterfugePerformedTextBox.Text);
+
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Subterfuge"] = subterfuge;
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0}<br />Average Acts of Subterfuge per Asset: {1:N2}<br />(Last Asset you Added: {2})",
+                elections.Sum(),
+                subterfuge.Average(),
+                assets[newIndex]);
+
+            assetNameTextBox.Text = "";
+            electionsRiggedTextBox.Text = "";
+            subterfugePerformedTextBox.Text = "";
+
+        }
+    }
+}
